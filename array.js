c0a166f40aa91a976eafc7cee55fa82a5434444b
gwanwoo-you <skymill2000@gmail.com>
2021-02-22 15:42:04 +0900

    array 실습

---
+++ array.js
@@ -0,0 +1,12 @@
+var car1 = "Saab";
+var car2 = "Volvo";
+var car3 = "BMW";
+
+var cars = ["Saab", "Volvo", "BMW", 123];
+//#in java
+//String [] cars = new Array[3];
+
+console.log(cars);
+// console.log(car[0]);
+// console.log(car[1]);
+// console.log(car[2]);
--- example03.js
+++ example03.js
@@ -2,6 +2,29 @@ function hello(){
     console.log("hello");
 }
 
+function plus(p1, p2){
+    return p1 + p2;
+}
+
+function minus(p1, p2){
+    return p1 - p2;
+}
+
+function div(p1, p2){
+    return p1 / p2;
+}
+
+function multi(p1, p2){
+    return p1 * p2;
+}
+
+console.log(plus(10));
+console.log(minus(10,5))
+console.log(div(10,5))
+console.log(multi(10,5))
+
+
+
 //es6 
 // const hello2 = () =>{
 //     console.log("hello2");
