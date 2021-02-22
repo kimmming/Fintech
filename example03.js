6048cb8b899b7f871e2119587933a253b3d5071e
gwanwoo-you <skymill2000@gmail.com>
2021-02-22 15:24:35 +0900

    #work1 사칙연산 기능만들기

---
+++ example03.js
@@ -0,0 +1,10 @@
+function hello(){
+    console.log("hello");
+}
+
+//es6 
+// const hello2 = () =>{
+//     console.log("hello2");
+// }
+
+//#work01 사칙연산을 하는 + - / * 기능을 작성하세요 plus , minus, div, multi
