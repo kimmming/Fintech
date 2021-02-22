f07222e83c0379bcf0f3208b3bbc09e4713a7881
gwanwoo-you <skymill2000@gmail.com>
2021-02-22 16:47:53 +0900

    동기 비동기 함수

--- async.js
+++ async.js
@@ -9,6 +9,7 @@ fs.readFile('./example/test.txt', 'utf8', function (err, result) {
     else {
         console.error("두번째 기능인데 파일을 읽어오느라 시간이... 조큼 ... 걸려요");
         console.log(result);
+        console.log('third func')
     }
 });
-console.log('third func')
+
